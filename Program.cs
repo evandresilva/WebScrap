590b3366ce20826b459f07fcb1b450ef04bfdfdd
Evandre Da Silva <evandresilva01@gmail.com>
2022-05-24 09:41:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WebScrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.bna.ao/#/");
+            IWebElement table = driver.FindElement(By.ClassName("text-capitalize"));
+
+            var moedas = table.FindElements(By.TagName("tr"));
+            foreach (var moeda in moedas)
+            {
+                var linha = moeda.Text;
+                Console.WriteLine($"{linha}");
+            }
+            Console.ReadKey();
+            driver.Quit();
+        }
+    }
+}
